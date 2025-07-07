5ce7266c5657f1bee6c24feb11e6ae87ce2da78c
Ngo Hieu <ngominhhieu10g1@gmail.com>
2025-07-07 08:21:10 +0700

    Config project successful while begin new create

--- src/main/java/com/webjob/application/HomeController.java
+++
@@ -1,2 +0,0 @@
-package com.webjob.application;public class HomeController {
-}
--- src/main/java/com/webjob/application/WebJobApplication.java
+++ src/main/java/com/webjob/application/WebJobApplication.java
@@ -1,13 +1,18 @@
 package com.webjob.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+
 public class WebJobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebJobApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebJobApplication.class, args);
+    }
 
 }
